70e0ec8c13ce6baaa99a6027ef07eeb139fa493d
handley <139220642+godvideo8899com@users.noreply.github.com>
2023-12-26 17:50:02 +0400

    url

--- utils/env.js
+++ utils/env.js
@@ -1,8 +1,12 @@
 "use strict";
 // 变量可自行添加修改
+let socketUrl = "http://154.92.15.136:8000/api";
+if (window&& window.location.hostname == "localhost") {
+	socketUrl = "http://192.178.223.143:8000/api";
+}
 export default { //存放变量的容器
 	appid: '',
-	baseUrl: 'http://154.92.15.136:8000/api',
+	baseUrl:socketUrl
     // baseUrl: 'http://192.178.223.143:8000/api',
 }
 /**
--- utils/socket.js
+++ utils/socket.js
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 let socketUrl = "http://154.92.15.136:9000";
-// if (window.location.hostname == "localhost") {
-//   socketUrl = "http://127.0.0.1:9000";
-// }
+if (window&&window.location.hostname == "localhost") {
+  socketUrl = "http://127.0.0.1:9000";
+}
 const socket = io(socketUrl);
 socket.on("connect", () => {
   console.log("连接成功");
