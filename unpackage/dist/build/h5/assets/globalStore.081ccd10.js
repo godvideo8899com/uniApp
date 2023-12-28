c5e521d21caad5a2435d73b5aa6e6f06a2449fb9
handley <139220642+godvideo8899com@users.noreply.github.com>
2023-12-28 19:03:20 +0400

    update

---
+++ unpackage/dist/build/h5/assets/globalStore.081ccd10.js
@@ -0,0 +1,1 @@
+import{p as t}from"./index-aada019a.js";const e=t([{text:"推荐",value:"1"},{text:"肉类",value:"2"},{text:"素菜",value:"3"},{text:"海鲜/鱼类",value:"4"},{text:"饮料/酒水",value:"5"},{text:"其他",value:"6"}]);export{e as s};
